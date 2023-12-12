1c75fe1e792157a24933f0a9ca9cfaa2db32d103
Craig Gant <craigkgant@gmail.com>
2023-12-12 12:59:57 -0500

    add styling options

--- src/utils/setColorThemeVariables.ts
+++ src/utils/setColorThemeVariables.ts
@@ -1,26 +1,38 @@
 import { TColorTheme } from '../types';
 
-const themes = {
+type TThemeValue = {
+	background: string;
+	border: string;
+	icon: string;
+	text: string;
+	[key: string]: string;
+};
+
+const themes: Record<TColorTheme, TThemeValue> = {
 	light: {
-		'--color-background': '#fff',
-		'--color-border': '#000',
-		'--color-icon': '#fff222',
-		'--color-text': '#000'
+		background: '#EAE2B7',
+		border: '#003049',
+		icon: '#003049',
+		text: '#003049'
 	},
 	dark: {
-		'--color-background': '#3F3F3F',
-		'--color-border': '#fff',
-		'--color-icon': '#aebebd',
-		'--color-text': '#88C8CB'
+		background: '#003049',
+		border: '#EAE2B7',
+		icon: '#EAE2B7',
+		text: '#EAE2B7'
 	}
 };
 
 const setColorThemeVariables = (theme: TColorTheme) => {
 	const { style } = document.documentElement;
 	const themeVariables = themes[theme];
+	const altTheme = theme === 'light' ? 'dark' : 'light';
+	const altThemeVariables = themes[altTheme];
 
 	Object.entries(themeVariables).forEach(([key, value]) => {
-		style.setProperty(key, value);
+		const altValue = altThemeVariables[key];
+		style.setProperty(`--color-${key}`, value);
+		style.setProperty(`--color-${key}-alt`, altValue);
 	});
 };
 
