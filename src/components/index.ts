b5d982bbe256c634fcfb67d0022254983df8ba2e
Craig Gant <craigkgant@gmail.com>
2023-12-19 08:09:06 -0500

    rename and streamline cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,14 @@
 import About from './About/About';
-import Card from './Card/Card';
+import EmploymentCard from './EmploymentCard/EmploymentCard';
 import ColorThemeSwitch from './ColorThemeSwitch/ColorThemeSwitch';
 import Header from './Header/Header';
 import { AnimatedMoon, AnimatedSun } from './Widgets';
 
-export { About, AnimatedMoon, AnimatedSun, Card, ColorThemeSwitch, Header };
+export {
+	About,
+	AnimatedMoon,
+	AnimatedSun,
+	ColorThemeSwitch,
+	EmploymentCard,
+	Header
+};
---
+++ src/utils/employmentHistory.ts
@@ -0,0 +1,62 @@
+import { TEmploymentHistory } from '../types';
+
+const employmentHistory: TEmploymentHistory[] = [
+	{
+		company: 'Mass Virtual',
+		companyLogo: '',
+		companyUrl: '',
+		endDate: 'Dec 2023',
+		id: '10',
+		startDate: 'Oct 2023',
+		taskDescriptions: [
+			'Built custom web applications that supported internal business needs.'
+		],
+		technologies: ['TypeScript', 'React', 'Next', 'MongoDB'],
+		title: 'Web Engineer'
+	},
+	{
+		company: 'Charter Communications',
+		companyLogo: '',
+		companyUrl: '',
+		endDate: 'Oct 2023',
+		id: '20',
+		startDate: 'Apr 2022',
+		taskDescriptions: [
+			'• Build accessible, scalable and secure full-stack web applications to meet a variety of business needs using React, Typescript, Node ,and Fastify.',
+			'• Architect and implement scalable database solutions for our applications. Integrate external applications and APIs (like Salesforce and Jira) into our apps.'
+		],
+		technologies: ['TypeScript', 'React', 'Node', 'Fastify', 'PostgreSQL'],
+		title: 'Software Engineer'
+	},
+	{
+		company: 'University of Central Florida',
+		companyLogo: '',
+		companyUrl: '',
+		endDate: 'Apr 2022',
+		id: '30',
+		startDate: 'Sep 2020',
+		taskDescriptions: [
+			'• Made custom REST applications using React, Vue, Node, Express, and both SQL and NoSQL databases',
+			'• Wrote unit and end-to-end tests with Jest and Cypress',
+			'• Converted legacy ColdFusion applications into modern JavaScript apps'
+		],
+		technologies: ['TypeScript', 'React', 'Next', 'MongoDB'],
+		title: 'Web Applications Developer'
+	},
+	{
+		company: 'Chec/Commerce.js',
+		companyLogo: '',
+		companyUrl: '',
+		endDate: 'Sep 2020',
+		id: '40',
+		startDate: 'Jun 2020',
+		taskDescriptions: [
+			'• Created eCommerce projects and integration guides using the Chec and Commerce.js SDKs and API.',
+			'• Built tutorials and proof of concept applications showing how one could use 3D technologies (like three.js and react-three-fiber) within a cart/checkout experience.'
+		],
+		technologies: ['React', 'Three.js'],
+		title: 'Developer Resources Contributor'
+	}
+];
+
+export default employmentHistory;
