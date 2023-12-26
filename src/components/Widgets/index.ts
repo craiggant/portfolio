275fb1cc6c851d91413bf9f26ef5bb74d14d6a52
Craig Gant <craigkgant@gmail.com>
2023-12-26 10:25:44 -0500

    add footer and fix left styles for scroll

--- src/components/Widgets/index.ts
+++ src/components/Widgets/index.ts
@@ -1,4 +1,5 @@
 import AnimatedMoon from './AnimatedMoon';
 import AnimatedSun from './AnimatedSun';
+import GithubIcon from './GithubIcon';
 
-export { AnimatedMoon, AnimatedSun };
+export { AnimatedMoon, AnimatedSun, GithubIcon };
