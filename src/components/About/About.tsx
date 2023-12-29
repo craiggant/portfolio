48bac8e196e2cb5323435149b27b742f012832c2
Craig Gant <craigkgant@gmail.com>
2023-12-29 08:32:38 -0500

    add copy enhancements

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,19 +8,19 @@ const About = () => {
 				<p>Hello there! 👋. </p>
 				<p>
 					I'm a passionate software engineer who loves crafting web
-					applications that aren't just functional, they're also also
-					a joy to use. I have proven experience building enterprise
-					level, user friendly, scalable, and secure web applications
-					– the kind that make life easier and work more productive.
+					applications that aren't just functional, they're also a joy
+					to use. I have proven experience building enterprise level,
+					user friendly, scalable and secure web applications – the
+					kind that make life easier and work more productive.
 				</p>
 				<br />
 				<p>
 					Whether collaborating in a dynamic team environment or
 					diving into a solo project, I bring a mix of technical
-					expertise, creative problem-solving, and a dash of humor to
+					expertise, creative problem-solving and a dash of humor to
 					the table. Let's connect and explore how we can make the
-					digital world more efficient, engaging, and delightful! 🚀
-					💻 🌟
+					digital world more efficient, engaging and delightful! 🚀 💻
+					🌟
 				</p>
 			</div>
 		</div>
--- src/utils/employmentHistory.ts
+++ src/utils/employmentHistory.ts
@@ -9,7 +9,7 @@ const employmentHistory: TEmploymentHistory[] = [
 		id: '10',
 		startDate: 'Oct 2023',
 		taskDescriptions: [
-			'• Built custom web applications that supported internal business needs.'
+			'- Built custom web applications that supported internal business needs.'
 		],
 		technologies: ['TypeScript', 'React', 'Next', 'MongoDB'],
 		title: 'Web Engineer'
@@ -19,12 +19,12 @@ const employmentHistory: TEmploymentHistory[] = [
 		companyLogo: '',
 		companyUrl: 'https://corporate.charter.com/',
 		endDate: 'Oct 2023',
-		notableAchievement: `• Single-handedly coded an application that takes in data from a Salesforce opportunity, transforms that data based on business logic and automatically creates multiple Jira tickets. \n • This application has saved tens of thousands of dollars for Charter, as it reduced the time sales personnel need to create these tickets from over 20 minutes to just a few seconds. Additionally, before the app, one full time employee was dedicated to the creation of these tickets. After the app, however, they were fully freed up to use their skills in other ways for the company.`,
+		notableAchievement: `- Single-handedly coded an application that takes in data from a Salesforce opportunity, transforms that data based on business logic and automatically creates multiple Jira tickets. \n - This application has saved tens of thousands of dollars for Charter, as it reduced the time sales personnel need to create these tickets from over 20 minutes to just a few seconds. Additionally, before the app, one full time employee was dedicated to the creation of these tickets. After the app, however, they were fully freed up to use their skills in other ways for the company.`,
 		id: '20',
 		startDate: 'Apr 2022',
 		taskDescriptions: [
-			'• Built accessible, scalable and secure full-stack web applications to meet a variety of business needs using React, Typescript, Node ,and Fastify.',
-			'• Architected and implemented scalable database solutions for our applications. Integrated external applications and APIs (like Salesforce and Jira) into our apps.'
+			'- Built accessible, scalable and secure full-stack web applications to meet a variety of business needs using React, Typescript, Node and Fastify.',
+			'- Architected and implemented scalable database solutions for our applications. Integrated external applications and APIs (like Salesforce and Jira) into our apps.'
 		],
 		technologies: ['TypeScript', 'React', 'Node', 'Fastify', 'PostgreSQL'],
 		title: 'Software Engineer'
@@ -34,13 +34,13 @@ const employmentHistory: TEmploymentHistory[] = [
 		companyLogo: '',
 		companyUrl: 'https://www.ucf.edu/',
 		endDate: 'Apr 2022',
-		notableAchievement: `• Coded an application using Next.js, Express and Socket.io that combed through the academic records of all current graduate students at the university and provided administrators with suggestions (based on school standards) about which students might need to be sent letters regarding their status with the school (placed on academic probation, be reinstated, etc.). The administrators were then able to use a common dashboard (built with web sockets) to collaborate and confirm which letters should be sent. Once confirmed, they could send all letters through the application.`,
+		notableAchievement: `- Coded an application using Next.js, Express and Socket.io that combed through the academic records of all current graduate students at the university and provided administrators with suggestions (based on school standards) about which students might need to be sent letters regarding their status with the school (placed on academic probation, be reinstated, etc.). The administrators were then able to use a common dashboard (built with web sockets) to collaborate and confirm which letters should be sent. Once confirmed, they could send all letters through the application.`,
 		id: '30',
 		startDate: 'Sep 2020',
 		taskDescriptions: [
-			'• Made custom REST applications using React, Vue, Node, Express, and both SQL and NoSQL databases',
-			'• Wrote unit and end-to-end tests with Jest and Cypress',
-			'• Converted legacy ColdFusion applications into modern JavaScript apps'
+			'- Made custom REST applications using React, Vue, Node, Express and both SQL and NoSQL databases',
+			'- Wrote unit and end-to-end tests with Jest and Cypress',
+			'- Converted legacy ColdFusion applications into modern JavaScript apps'
 		],
 		technologies: ['TypeScript', 'React', 'Next', 'MongoDB'],
 		title: 'Web Applications Developer'
@@ -53,8 +53,8 @@ const employmentHistory: TEmploymentHistory[] = [
 		id: '40',
 		startDate: 'Jun 2020',
 		taskDescriptions: [
-			'• Created eCommerce projects and integration guides using the Chec and Commerce.js SDKs and API.',
-			'• Built tutorials and proof of concept applications showing how one could use 3D technologies (like three.js and react-three-fiber) within a cart/checkout experience.'
+			'- Created eCommerce projects and integration guides using the Chec and Commerce.js SDKs and API.',
+			'- Built tutorials and proof of concept applications showing how one could use 3D technologies (like three.js and react-three-fiber) within a cart/checkout experience.'
 		],
 		technologies: ['React', 'Three.js'],
 		title: 'Developer Resources Contributor'
