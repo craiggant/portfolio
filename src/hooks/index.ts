e862d9ceb4f4a706fc2606ce9b4db0373562296f
Craig Gant <craigkgant@gmail.com>
2024-01-09 08:35:30 -0500

    add support for different screen widths on zoom in

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
+import useResize from './useResize';
 import useSetColorTheme from './useSetColorTheme';
 import useSize from './useSize';
 import useToggleColorTheme from './useToggleColorTheme';
 
-export { useSetColorTheme, useSize, useToggleColorTheme };
+export { useResize, useSetColorTheme, useSize, useToggleColorTheme };
